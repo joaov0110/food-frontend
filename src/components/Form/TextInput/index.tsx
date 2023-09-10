fa9732fce11126b692834e847f16c5f3460f1dfb
João Vitor <joaovitor3592@outlook.com>
2023-09-09 21:23:35 -0300

    featur: add TextIntput reusable component

---
+++ src/components/Form/TextInput/index.tsx
@@ -0,0 +1,46 @@
+import { FC, memo } from "react";
+import { TextField } from "@mui/material";
+
+import { Controller, useFormContext } from "react-hook-form";
+
+import "./index.scss";
+
+interface ItextInput {
+  name: string;
+  placeholder: string;
+  customChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TextInput: FC<ItextInput> = ({ name, placeholder, customChange }) => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field: { onChange, onBlur, value } }) => (
+        <TextField
+          value={value}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            onChange(e);
+            if (customChange) {
+              customChange(e);
+            }
+          }}
+          onBlur={onBlur}
+          placeholder={placeholder}
+          label={placeholder}
+          variant="filled"
+          error={!!errors[name]}
+          helperText={errors[name]?.message as string}
+          className="text-input"
+        />
+      )}
+    />
+  );
+};
+
+export default TextInput;
