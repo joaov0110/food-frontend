52ca65cf12beccf562fb69d9f8515f21bbbb66ae
João Vitor <joaovitor3592@outlook.com>
2023-09-12 01:19:05 -0300

    feature: create updateUser query

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,5 +1,6 @@
 import httpClient from "../config/axios";
-import { useQuery } from "react-query";
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import errorMessage from "../utils/errorMessage";
 
 const prefix = "/tenants";
 
@@ -14,7 +15,7 @@ interface Iaddress {
   updated_at: Date | null;
 }
 
-interface IgetUser {
+interface Iuser {
   id: number;
   name: string;
   document: string;
@@ -27,14 +28,68 @@ interface IgetUser {
   bgImage_url: string | null;
   created_at: Date;
   updated_at: Date | null;
+}
+
+interface IgetUser extends Iuser {
   address: Iaddress | null;
 }
 
+export type updateUser = Omit<
+  Iuser,
+  "id" | "is_new_tenant" | "created_at" | "updated_at"
+> &
+  Omit<Iaddress, "created_at" | "updated_at">;
+
 const useUser = () => {
+  const queryClient = useQueryClient();
+
   const getUser = async (): Promise<IgetUser> => {
     try {
       const { data } = await httpClient.get(`${prefix}/tenant`);
 
+      return data;
+    } catch (err: any) {
+      throw new Error(errorMessage(err));
+    }
+  };
+
+  const updateUser = async (payload: updateUser): Promise<string> => {
+    const {
+      name,
+      document,
+      email,
+      accountant_name,
+      accountant_email,
+      accountant_phone,
+      image_url,
+      bgImage_url,
+      postalCode,
+      street,
+      street_number,
+      district,
+      city,
+      UF,
+    } = payload;
+
+    try {
+      const { data } = await httpClient.put(`${prefix}/tenant`, {
+        name,
+        document,
+        email,
+        accountant_name,
+        accountant_email,
+        accountant_phone,
+        image_url,
+        bgImage_url,
+        address: {
+          postalCode,
+          street,
+          street_number,
+          district,
+          city,
+          UF,
+        },
+      });
       return data;
     } catch (err: any) {
       throw new Error(err);
@@ -43,6 +98,12 @@ const useUser = () => {
 
   return {
     user: useQuery<IgetUser, Error>({ queryKey: ["user"], queryFn: getUser }),
+
+    updateUser: useMutation(updateUser, {
+      onSuccess: () => {
+        queryClient.invalidateQueries("user");
+      },
+    }),
   };
 };
 
