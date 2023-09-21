472dd2b72f0fa0a788fa1bf6394fa10d86fcbb6e
João Vitor <joaovitor3592@outlook.com>
2023-09-21 03:01:03 -0300

    feature: create Point interface

---
+++ src/interfaces/pointInterface.ts
@@ -0,0 +1,13 @@
+import { Iaddress } from "./addressInterface";
+
+export interface IPoint {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  image_url?: string | null;
+  bgImage_url?: string | null;
+  created_at: Date;
+  updated_at: Date | null;
+  address: Iaddress | null;
+}
