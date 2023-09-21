f22dca11c834593c586b9f99cc30200133bfb1df
João Vitor <joaovitor3592@outlook.com>
2023-09-21 03:01:47 -0300

    feature: create Iaddress interface

---
+++ src/interfaces/addressInterface.ts
@@ -0,0 +1,10 @@
+export interface Iaddress {
+  postalCode: string;
+  street: string;
+  street_number: string;
+  district: string;
+  city: string;
+  UF: string;
+  created_at: Date;
+  updated_at: Date | null;
+}
