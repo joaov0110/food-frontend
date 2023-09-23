924cdd347e393ab85ced745635b346d81d734fae
João Vitor <joaovitor3592@outlook.com>
2023-09-23 17:12:36 -0300

    feature: create query constants

---
+++ src/constants/queries.ts
@@ -0,0 +1,10 @@
+export const GET_USER = "GET_USER";
+export const UPDATE_USER = "UPDATE_USER";
+export const POST_USER = "POST_USER";
+export const DELETE_USER = "DELETE_USER";
+
+export const GET_POINTS = "GET_POINTS";
+export const GET_POINT = "GET_POINT";
+export const UPDATE_POINT = "UPDATE_POINT";
+export const CREATE_POINT = "CREATE_POINT;";
+export const DELETE_POINT = "DELETE_POINT";
