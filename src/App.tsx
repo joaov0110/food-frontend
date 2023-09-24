7308df7a509148b3ace786dc40cbd89f043ea3d0
João Vitor <joaovitor3592@outlook.com>
2023-09-24 00:48:13 -0300

    feature: use WarningProvider and AppAlert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { WarningProvider } from "./contexts/warningContext";
 import "./App.scss";
+import AppAlert from "./components/Alert";
 
-function App() {
-  return <Outlet />;
-}
+const App = () => {
+  return (
+    <WarningProvider>
+      <AppAlert />
+      <Outlet />
+    </WarningProvider>
+  );
+};
 
 export default App;
