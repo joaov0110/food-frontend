79498a29d4defe24799f2342fa70566627b7affc
João Vitor <joaovitor3592@outlook.com>
2023-09-25 01:28:21 -0300

    feature: add image prop to CoverImage

--- src/components/CoverImage/index.tsx
+++ src/components/CoverImage/index.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
 import { Box } from "@mui/material";
 import { PhotoCameraBack } from "@mui/icons-material";
-
 import "./index.scss";
 
-const CoverImage: FC = () => {
+interface IcoverImage {
+  image: string;
+}
+
+const CoverImage: FC<IcoverImage> = ({ image }) => {
   return (
     <Box className="cover__container">
-      <img
-        className="cover__image"
-        src="https://www.bookhubpublishing.com/wp-content/uploads/revslider/the7-book-header/bg-slider-book-1500x750.jpg"
-        alt="bg image"
-      />
+      <img className="cover__image" src={image} />
       <div className="cover__overlay">
         <PhotoCameraBack color="primary" />
       </div>
