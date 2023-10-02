4f1f5d5f61628f256284afc92e79c54a72e492a6
João Vitor <joaovitor3592@outlook.com>
2023-10-02 04:31:52 -0300

    chore: add style prop to spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,19 +3,23 @@ import { FC } from "react";
 
 interface ICircularSpinner {
   size?: number;
+  styles?: {
+    [key: string]: string | number;
+  };
 }
 
 const cirularBoxStyles = {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  textAling: "center",
   width: "100%",
   height: "100vh",
 };
 
-const CircularSpinner: FC<ICircularSpinner> = ({ size }) => {
+const CircularSpinner: FC<ICircularSpinner> = ({ size, styles }) => {
   return (
-    <Box sx={cirularBoxStyles}>
+    <Box sx={{ ...cirularBoxStyles, ...styles }}>
       <CircularProgress size={size} />
     </Box>
   );
